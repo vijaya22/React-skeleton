766e41696b7fd77bc687704cf8374bea2d2cdfc0
vijaya22 <vijaya2798@gmail.com>
2022-09-26 13:22:06 +0530

    Skeleton react app intial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div contentEditable={true}></div>
+    </div>
+  );
+}
+
+export default App;
