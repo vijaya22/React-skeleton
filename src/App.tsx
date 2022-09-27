41efacfa4699e3c7dee9e4cafe74a5ce77a7b9c1
vijaya22 <vijaya2798@gmail.com>
2022-09-27 09:35:47 +0530

    Add a div in center of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <div contentEditable={true}></div>
+      <div>
+        <div
+          style={{
+            border: "solid 1px black",
+            backgroundColor: "black",
+            height: "100px",
+            width: "100px",
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
